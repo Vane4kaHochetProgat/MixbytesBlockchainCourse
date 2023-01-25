65288b4156716d79880e0c748808d9d3d5b7cbe6
Ivan Murashov <wolf.murashov@gmail.com>
2023-01-25 03:37:53 +0300

    appiarence fix

--- HW5/contracts/MyToken.sol
+++ HW5/contracts/MyToken.sol
@@ -4,14 +4,12 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract MyToken is ERC20Votes {
-    uint8 private constant DECIMALS = 6;
-    uint256 private constant INITIAL_SUPPLY = 100;
 
     constructor() ERC20("MyToken", "MYT") ERC20Permit("MyToken") {
-        _mint(msg.sender, INITIAL_SUPPLY * 10 ** DECIMALS);
+        _mint(msg.sender, 100 * 10 ** decimals());
     }
 
     function decimals() public view virtual override returns (uint8) {
-        return DECIMALS;
+        return 6;
     }
 }
